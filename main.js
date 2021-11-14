167b66549fcae8260db62470fb7c3d9215fcf322
Gvaroo <85008858+Gvaroo@users.noreply.github.com>
2021-11-14 19:34:13 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+var options = document.querySelectorAll(".options");
+var result = document.querySelector(".display-results");
+var clear = result.innerHTML;
+options.forEach(option => {
+  option.addEventListener("click", function () {
+    reset();
+    const pInput = this.value;
+    const cOptions = ["Rock", "Scissors", "Paper"];
+    const cInput = cOptions[Math.floor(Math.random() * 3)];
+    updateMoves(pInput, cInput);
+    compareInputs(pInput, cInput);
+
+  })
+})
+
+function updateMoves(pInput, cInput) {
+  document.getElementById("p-move").src = `assets/${pInput}.svg`;
+  document.getElementById("c-move").src = `assets/${cInput}.svg`;
+}
+
+function compareInputs(pInput, cInput) {
+  const currentMatch = `${pInput} vs ${cInput}`;
+  //TIE
+  if (pInput === cInput) {
+    var tie = `${currentMatch} is a tie`;
+    result.innerHTML += tie;
+
+    return;
+  }
+
+  //Rock
+  if (pInput == "Rock") {
+    if (cInput == "Scissors") {
+      var win = `${currentMatch} = u win!`;
+      result.innerHTML += win;
+
+    } else {
+      var lose = `${currentMatch} = Computer wins!`;
+      result.innerHTML += lose;
+
+    }
+  }
+
+  //Paper
+  else if (pInput == "Paper") {
+    if (cInput == "Rock") {
+      var win = `${currentMatch} = u win!`;
+      result.innerHTML += win;
+
+    } else {
+      var lose = `${currentMatch} = Computer wins!`;
+      result.innerHTML += lose;
+
+
+    }
+  }
+  //Scissors
+  else if (pInput == "Scissors") {
+    if (cInput == "Paper") {
+      var win = `${currentMatch} = u win!`;
+      result.innerHTML += win;
+
+
+    } else {
+      var lose = `${currentMatch} = Computer wins!`;
+      result.innerHTML += lose;
+
+    }
+  }
+}
+
+function reset() {
+  result.innerHTML = clear;
+}
+
+
